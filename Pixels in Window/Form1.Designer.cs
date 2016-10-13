d4308878f199f4796349da182fee8fde208390e8
ac <kit@kit.com>
2016-10-13 15:21:45 -0500

    Backgroud changed to blue

--- Pixels in Window/Form1.Designer.cs
+++ Pixels in Window/Form1.Designer.cs
@@ -44,6 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
             this.ClientSize = new System.Drawing.Size(282, 253);
             this.Controls.Add(this.pixelsLabel);
             this.Name = "Form1";
